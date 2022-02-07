1ad62b917d82d240f4a4da44a5fd24dc0d8000eb
Joel Ayala <ejayala2@gmail.com>
2022-02-07 16:07:37 -0500

    First commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=guswg5ZD89JCRh0Wp0lT8z0TN6uAUbTP`;
+    const response = await fetch(url);
+    const { data } = await response.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    });
+    console.log(gifs);
+    return gifs;
+}
